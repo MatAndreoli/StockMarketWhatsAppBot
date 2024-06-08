f48440659cefa553f27a909e8bb371838d436db2
MatAndreoli <andreoli.2003@gmail.com>
2024-06-08 10:40:00 -0300

    testing post

--- usecases/stocksData.js
+++ usecases/stocksData.js
@@ -71,7 +71,12 @@ const getStocksData = async (client, from, message) => {
       formData,
       {
         headers: {
+          ...formData.getHeaders(),
           'User-Agent': getRandom(),
+          Accept: 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+          'Accept-Language': 'en-US,en;q=0.9',
+          'Accept-Encoding': 'gzip, deflate, br',
+          Referer: 'https://statusinvest.com.br/',
         },
       }
     );
