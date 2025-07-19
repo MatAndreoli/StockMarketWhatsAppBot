fa094404416b01d8b86532c5ad846294e48a0cf7
MatAndreoli <andreoli.2003@gmail.com>
2025-07-19 15:49:54 -0300

    update whatsapp-web.js

--- index.js
+++ index.js
@@ -1,43 +1,45 @@
-const qrcode = require('qrcode-terminal');
-const onMessageCallback = require('./onMessage');
-const logger = require('./logger/loggerWinston');
-const Scheduler = require('./scheduler');
-const { Client, LocalAuth } = require('whatsapp-web.js');
+const qrcode = require("qrcode-terminal");
+const onMessageCallback = require("./onMessage");
+const logger = require("./logger/loggerWinston");
+const Scheduler = require("./scheduler");
+const { Client, LocalAuth } = require("whatsapp-web.js");
+
+console.log(process.env.URL_SHORTENER_URL)
 
 const client = new Client({
   puppeteer: {
-    args: ['--no-sandbox'],
+    args: ["--no-sandbox"],
   },
   authStrategy: new LocalAuth(),
 });
 
 client.initialize();
 
-client.on('authenticated', () => {
-  logger.info('Authenticated');
+client.on("authenticated", () => {
+  logger.info("Authenticated");
 });
 
-client.on('qr', (qr) => {
+client.on("qr", (qr) => {
   qrcode.generate(qr, { small: true });
 });
 
-client.on('ready', async () => {
-  logger.info('Client is ready!');
+client.on("ready", async () => {
+  logger.info("Client is ready!");
 
   try {
     const scheduler = new Scheduler(client);
 
     const { fiisFrequency, numbers } = scheduler.getSchedulerValues();
-    numbers.forEach(number => {
+    numbers.forEach((number) => {
       scheduler.scheduleFiisUpdate(number, fiisFrequency);
       // scheduler.scheduleStocksUpdate(number, frequency);
     });
   } catch (error) {
-    logger.error('Error setting up scheduler:', error);
+    logger.error("Error setting up scheduler:", error);
   }
 });
 
 client.on(
-  'message',
-  async (message) => await onMessageCallback(client, message)
+  "message",
+  async (message) => await onMessageCallback(client, message),
 );
